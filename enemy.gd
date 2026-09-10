e43229cdde48fde7384a3794ca4fe23de7b9f3fe
magnolia345 <your_email@example.com>
2026-09-09 21:54:18 -0400

    Enemy

--- enemy.gd
+++ enemy.gd
@@ -8,21 +8,22 @@ extends CharacterBody3D
 func _ready():
 	back.hide()
 func _physics_process(delta: float) -> void:
-
 	nav_agent.target_position = player.global_position
-	
-	# 2. Check if we've already reached the target
 	if nav_agent.is_navigation_finished():
 		back.show()
 		return
 
 	# 3. Get the next immediate vector point along the calculated path
 	var next_path_position: Vector3 = nav_agent.get_next_path_position()
-	
+	var next_path_pos = nav_agent.get_next_path_position()
 	# 4. Calculate the direction and velocity
 	var current_position: Vector3 = global_position
-	var new_velocity: Vector3 = (next_path_position - current_position).normalized() * 10
+	var new_velocity: Vector3 = (next_path_pos - current_position).normalized() * 10
 	
 	# 5. Move the CharacterBody3D
 	velocity = new_velocity
 	move_and_slide()
+	# 2. Check if we've already reached the target
+
+func _on_path_timer_timeout():
+	nav_agent.target_position = player.global_position
