93a6eb7f5ef5a071462d6374ff1b21b113e2a1c6
magnolia345 <your_email@example.com>
2026-09-10 21:32:10 -0400

    Enemy

--- enemy.gd
+++ enemy.gd
@@ -5,12 +5,15 @@ extends CharacterBody3D
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var player =  $"../player"# Update to your player's actual path
 @onready var back = $"../player/CanvasLayer/ImageForGodot"
+@onready var ray_cast: RayCast3D = $RayCast3D
+var player_in_vision_range: bool = false
 func _ready():
 	back.hide()
+	ray_cast.add_exception(self)
 func _physics_process(delta: float) -> void:
 	nav_agent.target_position = player.global_position
 	if nav_agent.is_navigation_finished():
-		back.show()
+		
 		return
 
 	# 3. Get the next immediate vector point along the calculated path
@@ -27,3 +30,29 @@ func _physics_process(delta: float) -> void:
 
 func _on_path_timer_timeout():
 	nav_agent.target_position = player.global_position
+func can_see_player() -> bool:
+	if not player_in_vision_range or not is_instance_valid(player):
+		return false
+		
+	# Target position is relative to the RayCast3D's global origin
+	ray_cast.target_position = player.global_position - global_position
+	
+	# Force physics frame precision check right now
+	ray_cast.force_raycast_update()
+	
+	if ray_cast.is_colliding():
+		var collider = ray_cast.get_collider()
+		if collider == player:
+			return true # Clear line of sight in 3D space!
+			
+	return false # A wall, pillar, or floor blocked the RayCast3D
+
+# --- Signal Connections from Area3D ---
+
+func _on_vision_area_body_entered(body: Node3D) -> void:
+	if body == player:
+		player_in_vision_range = true
+
+func _on_vision_area_body_exited(body: Node3D) -> void:
+	if body == player:
+		player_in_vision_range = false
